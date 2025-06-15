dd2082604279a045bce60cfa3faa125468e51be0
Sansan99999 <91546401+Sansan99999@users.noreply.github.com>
2025-06-16 03:56:08 +0700

    Create DiceAnimation.jsx

---
+++ src/components/DiceAnimation.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DiceAnimation() {
+  return (
+    <div className="my-4">
+      <p className="text-sm">Dice roll will be shown after transaction confirmed.</p>
+    </div>
+  );
+}
+
+export default DiceAnimation;
